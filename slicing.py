8fcccd295b79bf60eea191c40d5adcedd6c6b23e
aahnabiju <137191390+aahnabiju@users.noreply.github.com>
2023-11-18 20:19:28 -0800

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,18 @@
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+sliced_list = my_list[2:7]
+print("Sliced List 1:", sliced_list)
+
+sliced_list = my_list[:4]
+print("Sliced List 2:", sliced_list)
+
+sliced_list = my_list[5:]
+print("Sliced List 3:", sliced_list)
+
+sliced_list = my_list[1:9:2]
+print("Sliced List 4:", sliced_list)
+
+sliced_list = my_list[-3:]
+print("Sliced List 5:", sliced_list)
+
+sliced_list = my_list[::-1]
+print("Reversed List:", sliced_list)
